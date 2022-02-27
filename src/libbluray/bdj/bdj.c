e09484f8f02d7062c1feb98ec6dcc298cea571d1
MediaBrasil <MediaBrasil@users.noreply.github.com>
2022-02-27 09:35:38 -0300

    Update bdj.c

--- src/libbluray/bdj/bdj.c
+++ src/libbluray/bdj/bdj.c
@@ -406,12 +406,12 @@ static void *_load_jvm(const char **p_java_home, const char *app_java_home)
                                             "/usr/lib/jvm/default-java",
                                             "/usr/lib/jvm/default",
                                             "/usr/lib/jvm/",
-                                            "/etc/java-config-2/current-system-vm",
-                                            "/usr/lib/jvm/java-7-openjdk",
-                                            "/usr/lib/jvm/java-7-openjdk-" JAVA_ARCH,
+                                            "/etc/java-config-2/current-system-vm",                                            
                                             "/usr/lib/jvm/java-8-openjdk",
                                             "/usr/lib/jvm/java-8-openjdk-" JAVA_ARCH,
                                             "/usr/lib/jvm/java-6-openjdk",
+                                            "/usr/lib/jvm/java-11-openjdk",
+                                            "/usr/lib/jvm/java-11-openjdk-" JAVA_ARCH,
 #    endif
     };
     static const char * const jvm_dir[]  = {"jre/lib/" JAVA_ARCH "/server",
