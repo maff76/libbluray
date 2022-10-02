81e4d6e1b503c6b34a46c5fdd63cd6649086d240
MediaBrasil <MediaBrasil@users.noreply.github.com>
2022-10-02 16:45:00 -0300

    Update clpi_dump.c

--- src/devtools/clpi_dump.c
+++ src/devtools/clpi_dump.c
@@ -1,3 +1,4 @@
+13.40 KiB
 /*
  * This file is part of libbluray
  * Copyright (C) 2009-2010  John Stebbins
@@ -17,21 +18,15 @@
  * License along with this library. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <inttypes.h>
-
 #include "bluray.h"
 #include "bdnav/clpi_data.h"
-
 #include "util.h"
 #include "strings.h"
-
 static int verbose;
-
-
 const VALUE_MAP video_aspect_map[] = {
     {0, "Reserved1"},
     {1, "Reserved2"},
@@ -39,7 +34,6 @@ const VALUE_MAP video_aspect_map[] = {
     {3, "16:9"},
     {0, NULL}
 };
-
 const VALUE_MAP application_type_map[] = {
     {1, "Main TS for a main-path of Movie"},
     {2, "Main TS for a main-path of Time based slide show"},
@@ -51,7 +45,6 @@ const VALUE_MAP application_type_map[] = {
     {8, "Sub TS for a main-path of Enhanced LR View"},
     {0, NULL},
 };
-
 static void
 _show_stream(CLPI_PROG_STREAM *ss, int level)
 {
@@ -82,7 +75,6 @@ _show_stream(CLPI_PROG_STREAM *ss, int level)
                             ss->hdr_plus_flag ? "Yes" : "No");
             }
             break;
-
         case 0x03:
         case 0x04:
         case 0x80:
@@ -100,30 +92,25 @@ _show_stream(CLPI_PROG_STREAM *ss, int level)
                         _lookup_str(audio_rate_map, ss->rate));
             indent_printf(level, "Language: %s", ss->lang);
             break;
-
         case 0x90:
         case 0x91:
         case 0xa0:
             indent_printf(level, "Language: %s", ss->lang);
             break;
-
         case 0x92:
             indent_printf(level, "Char Code: %02x", ss->char_code);
             indent_printf(level, "Language: %s", ss->lang);
             break;
-
         default:
             fprintf(stderr, "unrecognized coding type %02x\n", ss->coding_type);
             break;
     };
 }
-
 static void
 _show_clip_info(CLPI_CL *cl, int level)
 {
     CLPI_CLIP_INFO *ci = &cl->clip;
     int ii;
-
     indent_printf(level, "Clip Info");
     indent_printf(level+1, "Clip Stream Type: %02x", ci->clip_stream_type);
     indent_printf(level+1, "Clip Application Type (%02x): %s",
@@ -150,17 +137,14 @@ _show_clip_info(CLPI_CL *cl, int level)
             indent_printf(level+2, "Font file %d: %s.otf", ii+1, cl->clip.font_info.font[ii].file_id);
         }
     }
-
     printf("\n");
 }
-
 static void
 _show_seq_info(CLPI_SEQ_INFO *si, int level)
 {
     CLPI_ATC_SEQ *atc;
     CLPI_STC_SEQ *stc;
     int ii, jj;
-
     indent_printf(level, "Sequence Info");
     indent_printf(level+1, "Number ATC Sequences: %d", si->num_atc_seq);
     for (ii = 0; ii < si->num_atc_seq; ii++) {
@@ -174,26 +158,24 @@ _show_seq_info(CLPI_SEQ_INFO *si, int level)
             indent_printf(level+2, "ATC Sequence %d", jj);
             indent_printf(level+3, "SPN STC Start: %u", stc->spn_stc_start);
             indent_printf(level+3, "PCR PID: %04x", stc->pcr_pid);
-            indent_printf(level+3, "Presentation Start: %u", 
+            indent_printf(level+3, "Presentation Start: %u",
                             stc->presentation_start_time);
-            indent_printf(level+3, "Presentation End: %u", 
+            indent_printf(level+3, "Presentation End: %u",
                             stc->presentation_end_time);
         }
     }
 }
-
 static void
 _show_prog_info(CLPI_PROG_INFO *pi, int level)
 {
     CLPI_PROG *prog;
     int ii, jj;
-
     indent_printf(level, "Program Info");
     indent_printf(level+1, "Number Programs: %d", pi->num_prog);
     for (ii = 0; ii < pi->num_prog; ii++) {
         prog = &pi->progs[ii];
         indent_printf(level+1, "Program %d", ii);
-        indent_printf(level+2, "SPN Program Sequence Start: %d", 
+        indent_printf(level+2, "SPN Program Sequence Start: %d",
                         prog->spn_program_sequence_start);
         indent_printf(level+2, "Program Map PID: %d", prog->program_map_pid);
         indent_printf(level+2, "Number Streams: %d", prog->num_streams);
@@ -204,20 +186,15 @@ _show_prog_info(CLPI_PROG_INFO *pi, int level)
         }
     }
 }
-
 static void
 _show_extent_start(CLPI_EXTENT_START *es, int level)
 {
     unsigned int ii;
-
     indent_printf(level, "Extension data: Extent Start Point");
-
     if (!es->num_point) {
         indent_printf(level+1, "(no data)");
-
     } else {
         indent_printf(level+1, "Number of Start Points: %d", es->num_point);
-
         if (verbose) {
             for (ii = 0; ii < es->num_point; ii++) {
                 indent_printf(level+1, "Extent %5d: SPN 0x%08X", ii, es->point[ii]);
@@ -225,7 +202,6 @@ _show_extent_start(CLPI_EXTENT_START *es, int level)
         }
     }
 }
-
 static void
 _show_cpi_info(CLPI_CPI *cpi, int level)
 {
@@ -233,7 +209,6 @@ _show_cpi_info(CLPI_CPI *cpi, int level)
     CLPI_EP_COARSE *coarse;
     CLPI_EP_FINE *fine;
     int ii, jj, kk;
-
     indent_printf(level, "CPI");
     indent_printf(level+1, "Number Stream PID: %d", cpi->num_stream_pid);
     for (ii = 0; ii < cpi->num_stream_pid; ii++) {
@@ -243,7 +218,7 @@ _show_cpi_info(CLPI_CPI *cpi, int level)
         indent_printf(level+2, "EP Stream Type: %d", entry->ep_stream_type);
         indent_printf(level+2, "Number EP Coarse: %d", entry->num_ep_coarse);
         indent_printf(level+2, "Number EP Fine: %d", entry->num_ep_fine);
-        indent_printf(level+2, "EP Map Start: %d", 
+        indent_printf(level+2, "EP Map Start: %d",
                         entry->ep_map_stream_start_addr);
         for (jj = 0; jj < entry->num_ep_coarse; jj++) {
             coarse = &entry->coarse[jj];
@@ -255,9 +230,9 @@ _show_cpi_info(CLPI_CPI *cpi, int level)
         for (jj = 0; jj < entry->num_ep_fine; jj++) {
             fine = &entry->fine[jj];
             indent_printf(level+2, "Fine: %d", jj);
-            indent_printf(level+3, "Angle Change Point: %s", 
+            indent_printf(level+3, "Angle Change Point: %s",
                 fine->is_angle_change_point ? "True":"False");
-            indent_printf(level+3, "I End Offset: %d", 
+            indent_printf(level+3, "I End Offset: %d",
                 fine->i_end_position_offset);
             indent_printf(level+3, "PTS EP: %d", fine->pts_ep);
             indent_printf(level+3, "SPN EP: %d", fine->spn_ep);
@@ -265,11 +240,9 @@ _show_cpi_info(CLPI_CPI *cpi, int level)
         if (verbose) {
             uint64_t pts;
             uint32_t spn;
-
             indent_printf(level+2, "PTS - SPN Map");
             for (jj = 0; jj < entry->num_ep_coarse; jj++) {
                 int start, end;
-
                 indent_printf(level+3, "Coarse: %d", jj);
                 coarse = &entry->coarse[jj];
                 start = coarse->ref_ep_fine_id;
@@ -290,13 +263,11 @@ _show_cpi_info(CLPI_CPI *cpi, int level)
         }
     }
 }
-
-
 static void
 _usage(char *cmd)
 {
-    fprintf(stderr, 
-"Usage: %s -vcspi <clpi file> [<clpi file> ...]\n"
+    fprintf(stderr,
+"Usage: %s [options] <clpi file> [<clpi file> ...]\n"
 "With no options, produces no output (not very useful)\n"
 "Options:\n"
 "    v - Verbose output.\n"
@@ -306,12 +277,9 @@ _usage(char *cmd)
 "    i - Shows the CPI. PTS to SPN map\n"
 "    e - Shows Extent Start Table\n"
 , cmd);
-
     exit(EXIT_FAILURE);
 }
-
 #define OPTS "vcspie"
-
 int
 main(int argc, char *argv[])
 {
@@ -320,46 +288,36 @@ main(int argc, char *argv[])
     int opt_clip_info = 0, opt_seq_info = 0, opt_prog_info = 0;
     int opt_cpi_info = 0, opt_extent_start = 0;
     int ii;
-
     do {
         opt = getopt(argc, argv, OPTS);
         switch (opt) {
             case -1: break;
-
             case 'v':
                 verbose = 1;
                 break;
-
             case 's':
                 opt_seq_info = 1;
                 break;
-
             case 'i':
                 opt_cpi_info = 1;
                 break;
-
             case 'c':
                 opt_clip_info = 1;
                 break;
-
             case 'p':
                 opt_prog_info = 1;
                 break;
-
             case 'e':
                 opt_extent_start = 1;
                 break;
-
             default:
                 _usage(argv[0]);
                 break;
         }
     } while (opt != -1);
-
     if (optind >= argc) {
         _usage(argv[0]);
     }
-
     for (ii = optind; ii < argc; ii++) {
         cl = bd_read_clpi(argv[ii]);
         if (cl == NULL) {
@@ -382,7 +340,6 @@ main(int argc, char *argv[])
             // Show cpi
             _show_cpi_info(&cl->cpi, 1);
         }
-
         if (opt_prog_info) {
             if (cl->program_ss.num_prog) {
                 printf("\n");
@@ -403,9 +360,7 @@ main(int argc, char *argv[])
                 _show_extent_start(&cl->extent_start, 1);
             }
         }
-
         bd_free_clpi(cl);
     }
     return 0;
 }
-
