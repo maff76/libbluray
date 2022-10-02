4bace8c09eb21d4112f99bd9d1ed37ff2e666a93
MediaBrasil <MediaBrasil@users.noreply.github.com>
2022-10-02 17:06:30 -0300

    Update keys.h

--- src/libbluray/keys.h
+++ src/libbluray/keys.h
@@ -1,7 +1,6 @@
 /*
  * This file is part of libbluray
- * Copyright (C) 2009-2010  Obliter0n
- * Copyright (C) 2009-2010  John Stebbins
+ * Copyright (C) 2010-2022  Petri Hintukainen <phintuka@users.sourceforge.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,79 +19,58 @@
 
 /**
  * @file
- * \brief Log control and capture
- *
- * Logging level can be changed with function bd_set_debug_mask() or environment variable BD_DEBUG_MASK.
- * Default is to log only errors and critical messages (DBG_CRIT).
- *
- * Application can capture log messages with bd_set_debug_handler().
- * Messages can be written to a log file with BD_DEBUG_FILE environment variable.
- * By default messages are written to standard error output.
+ * \brief User input key definitions
  */
 
-#ifndef BD_LOG_CONTROL_H_
-#define BD_LOG_CONTROL_H_
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdint.h>
+#if !defined(_BD_KEYS_H_)
+#define _BD_KEYS_H_
 
 /**
- * Flags for log filtering.
+ * Key codes
  */
-enum debug_mask_enum {
-    DBG_RESERVED   = 0x00001,
-    DBG_CONFIGFILE = 0x00002, /*   (reserved for libaacs) */
-    DBG_FILE       = 0x00004, /*   (reserved for libaacs) */
-    DBG_AACS       = 0x00008, /*   (reserved for libaacs) */
-    DBG_MKB        = 0x00010, /*   (reserved for libaacs) */
-    DBG_MMC        = 0x00020, /*   (reserved for libaacs) */
-    DBG_BLURAY     = 0x00040, /**< BluRay player */
-    DBG_DIR        = 0x00080, /**< Directory access */
-    DBG_NAV        = 0x00100, /**< Database files (playlist and clip info) */
-    DBG_BDPLUS     = 0x00200, /*   (reserved for libbdplus) */
-    DBG_DLX        = 0x00400, /*   (reserved for libbdplus) */
-    DBG_CRIT       = 0x00800, /**< **Critical messages and errors** (default) */
-    DBG_HDMV       = 0x01000, /**< HDMV virtual machine execution trace */
-    DBG_BDJ        = 0x02000, /**< BD-J subsystem and Xlet trace */
-    DBG_STREAM     = 0x04000, /**< m2ts stream trace */
-    DBG_GC         = 0x08000, /**< graphics controller trace */
-    DBG_DECODE     = 0x10000, /**< PG / IG decoders, m2ts demuxer */
-    DBG_JNI        = 0x20000, /**< JNI calls */
-};
+typedef enum {
+    BD_VK_NONE      = 0xffff,  /**< no key pressed */
 
-typedef enum debug_mask_enum debug_mask_t;
+    /* numeric key events */
+    BD_VK_0         = 0,   /**< "1" */
+    BD_VK_1         = 1,   /**< "2" */
+    BD_VK_2         = 2,   /**< "3" */
+    BD_VK_3         = 3,   /**< "4" */
+    BD_VK_4         = 4,   /**< "5" */
+    BD_VK_5         = 5,   /**< "6" */
+    BD_VK_6         = 6,   /**< "7" */
+    BD_VK_7         = 7,   /**< "8" */
+    BD_VK_8         = 8,   /**< "9" */
+    BD_VK_9         = 9,   /**< "0" */
 
-typedef void (*BD_LOG_FUNC)(const char *);
+    /* */
+    BD_VK_ROOT_MENU = 10,  /**< Open disc root menu */
+    BD_VK_POPUP     = 11,  /**< Toggle popup menu */
 
-/**
- * Set (global) debug handler
- *
- * The function will receive all enabled log messages.
- *
- * @param handler function that will receive all enabled log and trace messages
- *
+    /* interactive key events */
+    BD_VK_UP        = 12,  /**< Arrow up */
+    BD_VK_DOWN      = 13,  /**< Arrow down */
+    BD_VK_LEFT      = 14,  /**< Arrow left */
+    BD_VK_RIGHT     = 15,  /**< Arrow right */
+    BD_VK_ENTER     = 16,  /**< Select */
+
+    /** Mouse click. Translated to BD_VK_ENTER if mouse is over a valid button. */
+    BD_VK_MOUSE_ACTIVATE = 17,
+
+    BD_VK_RED       = 403, /**< Color key "Red" */
+    BD_VK_GREEN     = 404, /**< Color key "Green" */
+    BD_VL_YELLOW    = 405, /**< Color key "Yellow" */
+    BD_VK_BLUE      = 406, /**< Color key "Blue" */
+
+} bd_vk_key_e;
+
+/*
+ * Application may optionally provide KEY_PRESSED, KEY_TYPED and KEY_RELEASED events.
+ * These masks are OR'd with the key code when calling bd_user_input().
  */
-void bd_set_debug_handler(BD_LOG_FUNC handler);
 
-/**
- * Set (global) debug mask
- *
- * @param mask combination of flags from debug_mask_enum
- */
-void bd_set_debug_mask(uint32_t mask);
+#define BD_VK_KEY_PRESSED   0x80000000   /**< Key was pressed down */
+#define BD_VK_KEY_TYPED     0x40000000   /**< Key was typed */
+#define BD_VK_KEY_RELEASED  0x20000000   /**< Key was released */
 
-/**
- * Get current (global) debug mask
- *
- * @return combination of flags from debug_mask_enum
- */
-uint32_t bd_get_debug_mask(void);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* BD_LOG_CONTROL_H_ */
+#endif // _BD_KEYS_H_
