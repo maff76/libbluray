a6816f3bf41e244d97312c7e4956be3ed4952f7f
MediaBrasil <MediaBrasil@users.noreply.github.com>
2022-10-05 13:14:19 -0300

    Update bluray.c

--- src/libbluray/bluray.c
+++ src/libbluray/bluray.c
@@ -1,4 +1,4 @@
- * ANY WARRANTY; without even the implied warranty of
+/* ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
